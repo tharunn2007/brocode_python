0392795386dd0d0e30dc22183763a32713b8a903
Tharunn D <dtharunn@gmail.com>
2025-11-04 12:02:52 +0530

    Edited input and started with math functions

--- 12HRLECTURE.py
+++ 12HRLECTURE.py
@@ -24,6 +24,10 @@ else:#here else is used to give an alternative condition if the if condition is 
     print("Boring")
 
 
+
+
+
+
 #type casting:converting one data type to another data type
 string_to_integer="12345"#string value
     #can be converted with these functions like str(),int(),float(),bool()
@@ -44,13 +48,59 @@ print(x+y)  #----- gives OUTPUT:4.14 as a float type
 
 
 
+
+
+
+
 #input() is a function that takes in input from user to be entered....with thin the quotes string sentence can be included
 name_input=input("What is your name:")# I can give my name as Tharunn
 print(f"Hello there {name_input}!")#------ PRINTS Hello there Tharunn!
 #note:normally input() function takes in input as a string in default unless mentioned but we can change it
-# age_input=int(input("Enter you age:"))
-#mark_input= float(input("Enter you marks:"))
-#For boolean cases:
+age_input=int(input("Enter you age:"))
+mark_input= float(input("Enter you marks:"))
+
+
+
+
+
+#~~~~~~~~~PROJECT:MADLIBS~~~~~~~#
+#madlibs:you mke up a story using the nouns,adverb,adjective and sort of things likt that and make up a story
+adjective1=input("Enter adjective1:")
+adjective2=input("Enter adjective2:")
+adverb=input("Enter adverb:")
+noun=input("Enter noun:")
+print(f"Yesterday,I went to {noun}'s house which was very {adjective1}.His mom gave me a glass of juice which was {adverb} and also spoke {adjective2} German.")
+#OUTPUT:Yesterday, I went to Arjun's house which was very huge.His mom gave me a glass of juice which was tasty and also spoke good German.
+#~~~~~~~~~~OVER~~~~~~~~~~~#
+
+
+
+
+
+
+#truncating decimal places using round()function.
+Numerator=10
+Denominator=3
+Division1_gives=Numerator/Denominator
+print(Division1_gives)#~~~~~~~OUTPUT= 3.3333333333333335
+rounded_Division1_gives=round(Division1_gives,2)#Rounding off to 2 Decimal places
+print(rounded_Division1_gives)#~~~~~~~~~OUTPUT= 3.33
+
+
+
+
+
+
+#MATH AND MATH MODULE IN PYTHON AND HOW TO USE IT:
+#topics:arithmetic operators,math functions and few exercises
+
+
+
+
+
+
+
+
 
 
 
